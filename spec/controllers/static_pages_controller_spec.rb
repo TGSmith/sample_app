7a71d5c8fadc97b8d7e8790837d358c9a448991f
Tristan Smith <Tristan13smith@gmail.com>
2013-02-13 00:37:12 +0000

    Make a basic User model (including secure passwords)

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe StaticPagesController do
   describe "GET '...'" do
     it "returns http success" do
-      get '...'
+      "get '...'"
       response.should be_success
     end
   end
